a5f00a131528dd27c2476da751b5f097598ff649
Mohcine Ghalmi <mghalmi@e1r7p2.1337.ma>
2025-03-06 21:21:43 +0000

    first jeff

---
+++ test/Re-entrancy.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+
+import "openzeppelin-contracts-06/math/SafeMath.sol";
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Reentrance} from "../src/Re-entrancy.sol";
+
+contract Reentrance is Test {
+
+}
